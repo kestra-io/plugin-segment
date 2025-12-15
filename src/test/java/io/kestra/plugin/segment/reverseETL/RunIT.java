de6440024c874ea86ec69f0255d48e9db7e585a7
Malay Dewangan <malaydewangan310@gmail.com>
2025-12-15 11:12:08 +0530

    feat(segment): introduce new reverse ETL tasks

---
+++ src/test/java/io/kestra/plugin/segment/reverseETL/RunIT.java
@@ -0,0 +1,35 @@
+package io.kestra.plugin.segment.reverseETL;
+
+import io.kestra.core.junit.annotations.KestraTest;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.runners.RunContextFactory;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@KestraTest
+@EnabledIfEnvironmentVariable(named = "SEGMENT_TOKEN", matches = ".+")
+class RunIT {
+    @Inject
+    private RunContextFactory runContextFactory;
+
+    @Test
+    void shouldTriggerReverseEtlSync() throws Exception {
+        Run task = Run.builder()
+            .token(Property.ofValue(System.getenv("SEGMENT_TOKEN")))
+            .sourceId(Property.ofValue("source-id"))
+            .modelId(Property.ofValue("model-id"))
+            .subscriptionId(Property.ofValue("subscription-id"))
+            .wait(Property.ofValue(false))
+            .build();
+
+        RunContext runContext = runContextFactory.of();
+
+        Run.Output output = task.run(runContext);
+
+        assertThat(output.getSyncId()).isNotNull();
+    }
+}
